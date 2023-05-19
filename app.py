21e7f03ccf5e860999a7a03ccb9e60ed1c51859a
unknown <marmoljorge98@gmail.com>
2023-05-19 10:20:03 +0200

    My APP4

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import dash
+#from dash import dcc
+#from dash import html
+#from dash import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
@@ -14,6 +17,8 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 # DATA
+
+#df = pd.read_parquet('./social_network.parquet')
 df = pd.read_parquet('/tmp/social_network.parquet')
 
 # LAYOUT
@@ -73,7 +78,14 @@ app.layout = html.Div([
                 id='twitter-visit',
             )
         ]),
-    ], style={"columnCount": 4, 'textAlign': "center"}),
+        html.Div([
+            html.Img(src="https://pngimg.com/uploads/twitch/twitch_PNG46.png",
+                     style={"width": "100px"}),
+            html.H2(
+                id='twitch-visit',
+            )
+        ])
+    ], style={"columnCount": 5, 'textAlign': "center"}),
     html.H3('Total Visits by Month', style={"textAlign": "center"}),
     dcc.Graph(
         id='total-visit-line'
@@ -100,6 +112,7 @@ app.layout = html.Div([
     Output('facebook-visit', 'children'),
     Output('instagram-visit', 'children'),
     Output('twitter-visit', 'children'),
+    Output('twitch-visit', 'children'),
     Output('total-visit-line', 'figure'),
     Output('total-visit-social-networks-line', 'figure'),
     Output('world-map', 'figure'),
@@ -139,6 +152,15 @@ def update_figures(start_date_selected, end_date_selected, social_networks_selec
     twitter_visit = (
         df
         .loc[(df.social_network == 'twitter') &
+             (df.social_network.isin(social_networks_selected)) &
+             (df.device.isin(devices_selected)) &
+             (df.datetime >= start_date_selected) &
+             (df.datetime <= end_date_selected)]
+    ).shape[0]
+
+    twitch_visit = (
+        df
+        .loc[(df.social_network == 'twitch') &
              (df.social_network.isin(social_networks_selected)) &
              (df.device.isin(devices_selected)) &
              (df.datetime >= start_date_selected) &
@@ -238,7 +260,7 @@ def update_figures(start_date_selected, end_date_selected, social_networks_selec
         }
     )
 
-    return total_visit, facebook_visit, instagram_visit, twitter_visit, total_visit_fig, total_visit_social_network_fig, world_map_fig, devices_pie_fig
+    return total_visit, facebook_visit, instagram_visit, twitter_visit,twitch_visit, total_visit_fig, total_visit_social_network_fig, world_map_fig, devices_pie_fig
 
 
 if __name__ == '__main__':
